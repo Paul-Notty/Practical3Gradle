7a3b2a750ae9b9d2f0dde67b0486e0f5f148ca52
Paul-Notty <pan00079@students.stir.ac.uk>
2025-09-30 20:57:17 +0100

    init

---
+++ src/main/java/practical1/Practical1.java
@@ -0,0 +1,8 @@
+package practical1;
+
+public class Practical1 {
+    public static void main(String[] args) {
+	System.out.println("Hello, I am the bairn. I love Brad Spencer. Kyle is a Knob");
+    System.out.println("Unbelieeeeeeeeeeeeeeeeevable");
+    }
+}
